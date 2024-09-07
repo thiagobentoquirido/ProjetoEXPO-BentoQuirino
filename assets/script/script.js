5634f4ca8cfc6175ce3183ff1bc36211f9ca5153
thiagobentoquirido <thiagocontaazr123@gmail.com>
2024-09-07 11:41:16 -0300

    Add files via upload

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,35 @@
 
+const baseUrl = "https://cataas";
 
+
+async function getData() {
+    let response = await fetch(`https://cataas.com/cat`);
+    response = await response.blob();
+    console.log(response);
+    return URL.createObjectURL(response);
+    
+    
+}
+async function plotData() {
+  const quots = await getData();
+  let div = document.getElementById('catimg')
+  div.innerHTML = `<img src="${quots}" />`;
+  window.location.reload(true);
+  
+}
+
+/*
 function fbt(){
     let imagemslv = localStorage.getItem('imagemslv');
     let img = document.getElementById("catimg");
   //  const response = await fetch('https://cataas.com/cat');
 
-    window.location.reload(true);
+    
     
     
     
 }
 
-//window.onload = fbt;
+//
 const btn = document.getElementById("BT");
-btn.addEventListener("click", fbt());
+btn.addEventListener("click", fbt());*/
